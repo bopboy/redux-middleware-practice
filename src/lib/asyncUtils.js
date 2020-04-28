091cf0eead06b39bfa0012f323ff0a8ea6cf5bce
bopboy2 <scp0000@gmail.com>
2020-04-28 22:46:39 +0900

    07 refactoring 2

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -4,3 +4,16 @@ export const reducerUtils = {
     success: data => ({ data, loading: false, error: null }),
     error: error => ({ data: null, loading: true, error })
 };
+
+export const createPromiseThunk = (type, promiseCreater) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return param => async dispatch => {
+        dispatch({ type });
+        try {
+            const payload = await promiseCreater(param);
+            dispatch({ type: SUCCESS, payload });
+        } catch (e) {
+            dispatch({ type: ERROR, payload: e, error: true });
+        }
+    }
+}
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postAPI from '../api/posts';
-import { reducerUtils } from '../lib/asyncUtils';
+import { reducerUtils, createPromiseThunk } from '../lib/asyncUtils';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -9,24 +9,8 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-export const getPosts = () => async dispatch => {
-    dispatch({ type: GET_POSTS });
-    try {
-        const posts = await postAPI.getPosts();
-        dispatch({ type: GET_POSTS_SUCCESS, posts });
-    } catch (e) {
-        dispatch({ tpye: GET_POSTS_ERROR, error: e });
-    }
-}
-export const getPost = () => async dispatch => {
-    dispatch({ type: GET_POST });
-    try {
-        const post = await postAPI.getPostById();
-        dispatch({ type: GET_POST_SUCCESS, post });
-    } catch (e) {
-        dispatch({ tpye: GET_POST_ERROR, error: e });
-    }
-}
+export const getPosts = createPromiseThunk(GET_POSTS, postAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postAPI.getPostById);
 
 const initialState = {
     posts: reducerUtils.initial(),
@@ -36,11 +20,11 @@ const initialState = {
 export default function posts(state = initialState, action) {
     switch (action.type) {
         case GET_POSTS: return { ...state, posts: reducerUtils.loading() };
-        case GET_POSTS_SUCCESS: return { ...state, posts: reducerUtils.success(action.posts) };
-        case GET_POSTS_ERROR: return { ...state, posts: reducerUtils.error(action.error) };
+        case GET_POSTS_SUCCESS: return { ...state, posts: reducerUtils.success(action.payload) };
+        case GET_POSTS_ERROR: return { ...state, posts: reducerUtils.error(action.payload) };
         case GET_POST: return { ...state, post: reducerUtils.loading() };
-        case GET_POST_SUCCESS: return { ...state, post: reducerUtils.success(action.post) };
-        case GET_POST_ERROR: return { ...state, post: reducerUtils.error(action.error) };
+        case GET_POST_SUCCESS: return { ...state, post: reducerUtils.success(action.payload) };
+        case GET_POST_ERROR: return { ...state, post: reducerUtils.error(action.payload) };
         default: return state;
     }
 }
