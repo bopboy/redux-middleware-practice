d5a43985d9fc3833e2853ab625c455e042e77424
bopboy2 <scp0000@gmail.com>
2020-04-28 22:39:26 +0900

    06-2 refactoring 1

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postAPI from '../api/posts';
+import { reducerUtils } from '../lib/asyncUtils';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -28,18 +29,18 @@ export const getPost = () => async dispatch => {
 }
 
 const initialState = {
-    posts: { loading: false, data: null, error: null },
-    post: { loading: false, data: null, error: null }
+    posts: reducerUtils.initial(),
+    post: reducerUtils.initial()
 }
 
 export default function posts(state = initialState, action) {
     switch (action.type) {
-        case GET_POSTS: return { ...state, posts: { loading: true, data: null, error: null } };
-        case GET_POSTS_SUCCESS: return { ...state, posts: { loading: false, data: action.posts, error: null } };
-        case GET_POSTS_ERROR: return { ...state, posts: { loading: false, data: null, error: action.error } };
-        case GET_POST: return { ...state, post: { loading: true, data: null, error: null } };
-        case GET_POST_SUCCESS: return { ...state, post: { loading: false, data: action.posts, error: null } };
-        case GET_POST_ERROR: return { ...state, post: { loading: false, data: null, error: action.error } };
+        case GET_POSTS: return { ...state, posts: reducerUtils.loading() };
+        case GET_POSTS_SUCCESS: return { ...state, posts: reducerUtils.success(action.posts) };
+        case GET_POSTS_ERROR: return { ...state, posts: reducerUtils.error(action.error) };
+        case GET_POST: return { ...state, post: reducerUtils.loading() };
+        case GET_POST_SUCCESS: return { ...state, post: reducerUtils.success(action.post) };
+        case GET_POST_ERROR: return { ...state, post: reducerUtils.error(action.error) };
         default: return state;
     }
 }
