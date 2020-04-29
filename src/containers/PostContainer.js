4e62e2a53a318108da376930d495d1a6cff9f60d
bopboy2 <scp0000@gmail.com>
2020-04-29 22:30:44 +0900

    12 개선 2

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
-import { getPost, clearPost } from '../modules/posts';
+import { getPost } from '../modules/posts';
+import { reducerUtils } from "../lib/asyncUtils";
 
 function PostContainer({ postId }) {
-    const { data, loading, error } = useSelector(state => state.posts.post);
+    const { data, loading, error } = useSelector(state => state.posts.post[postId] || reducerUtils.initial());
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getPost(postId));
-        return () => { dispatch(clearPost()) };
+        // return () => { dispatch(clearPost()) };
     }, [postId, dispatch]);
 
-    if (loading) return <div>loading...</div>;
+    if (loading && !data) return <div>loading...</div>;
     if (error) return <div> somgthing wrong!!!</div>;
     if (!data) return null
 
