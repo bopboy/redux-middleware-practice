8f59b0bb96a22d946fe0ba6bd740e716ef189a74
bopboy2 <scp0000@gmail.com>
2020-04-30 10:40:06 +0900

    15 using json-server

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,17 +1,11 @@
-const sleep = n => new Promise(resolve => setTimeout(resolve, n));
-
-const posts = [
-    { id: 1, title: 'react', description: '리액트 배우기' },
-    { id: 2, title: 'redux', description: '리덕스 배우기' },
-    { id: 3, title: 'javascript', description: '자바스크립트 배우기' }
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-    await sleep(500);
-    return posts;
+    const response = await axios.get('http://localhost:4000/posts');
+    return response.data;
 };
 
 export const getPostById = async id => {
-    await sleep(500);
-    return posts.find(post => post.id === id);
+    const response = await axios.get(`http://localhost:4000/posts/${id}`);
+    return response.data;
 };
